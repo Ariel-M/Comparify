c59182cade2bc410e953393bde9b53358114fa7a
Ariel Moskowitz <arielsm86@gmail.com>
2018-08-12 18:28:35 -0400

    Rearranged files and added comments

--- main.py
+++ main.py
@@ -18,14 +18,18 @@ jinja_env = jinja2.Environment(
 class ApiHandler(webapp2.RequestHandler):
     def get(self):
         home_template = jinja_env.get_template('index.html')
+        # Searches Walmart's database for the query entered by the user 
         call_walmart_api = requests.get('http://api.walmartlabs.com/v1/search?apiKey=t29nkcuug33kqst5r2b53d9z&query=ipod')
         walmart_json = call_walmart_api.json()
+
+        # sets the variable to the first item that comes up's upc number
         walmart_upc = int(walmart_json[u'items'][1][u'upc'])
         
+        # finds the product information 
         walmart_item_api = requests.get('http://api.walmartlabs.com/v1/items?apiKey=t29nkcuug33kqst5r2b53d9z&upc=%s' % (walmart_upc))
         walmart_item_json = walmart_item_api.json()
          
-
+        # finds product information from ebay's database using the upc code from Walmart
         ebay_item_api = requests.get('http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByProduct&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=ScottMos-Comparif-PRD-1ed499e41-3b97c9fb&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=2&productId.@type=UPC&productId=%s' % (walmart_upc))
         ebay_item_json = ebay_item_api.json()
         self.response.write(ebay_item_json)
