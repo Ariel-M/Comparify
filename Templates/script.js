4801a33716d09b4a62ddc6d429df46fa24435865
Muhammad Hamza Niaz <hamzaniaz9856@gmail.com>
2018-08-14 15:40:27 -0400

    Added highlight to the lower price

--- Templates/script.js
+++ Templates/script.js
@@ -1,7 +1,18 @@
-let
-
-if price(box_a < box_b) {
-    print ""
-} else {
-
+let Walmart_Price =
+let Ebay_Price =
+hightlight()
+if (Walmart_Price > Ebay_Price) {
+  console.log(Ebay_Price);
 }
+else {
+  (Walmart_Price);
+}
+function hightlight(text){
+  let price = document.query.Selector();
+  let innerHTML = price.innerHTML;
+  if {
+   innerHTML = innerHTML.substring(0,index) + "<div class='price'>" + innerHTML.substring(index,index+text.float) + "</div>" + innerHTML.substring(index + text.float);
+   price.innerHTML = innerHTML;
+  }
+}
+return hightlight()
--- main.py
+++ main.py
@@ -26,57 +26,57 @@ class ApiHandler(webapp2.RequestHandler):
         result_template = jinja_env.get_template('Templates/Display_Page.html')
         search = self.request.get('query')
         popular_search = 0
-        
+
         record = None
         entry_exists = False
-        
+
         for entry in SearchTerms.query().order(SearchTerms.search_term).fetch():
             if search == entry.search_term:
-                
+
                 entry.search_frequency += 1
                 entry.put()
-                
+
                 entry_exists = True
                 record = entry
                 break
-        
+
             else:
                 search_entry = SearchTerms(search_term = search, search_frequency = popular_search)
                 search_entry.put()
                 break
-              
-            
-                   
-                
-                
 
 
-        # Searches Walmart's database for the query entered by the user 
+
+
+
+
+
+        # Searches Walmart's database for the query entered by the user
         call_walmart_api = requests.get('http://api.walmartlabs.com/v1/search?apiKey=t29nkcuug33kqst5r2b53d9z&query=%s' % (search))
         walmart_json = call_walmart_api.json()
 
         # sets the variable to the first item that comes up's upc number
         walmart_upc = int(walmart_json[u'items'][1][u'upc'])
-        
-        # finds the product information 
+
+        # finds the product information
         walmart_item_api = requests.get('http://api.walmartlabs.com/v1/items?apiKey=t29nkcuug33kqst5r2b53d9z&upc=%s' % (walmart_upc))
         walmart_item_json = walmart_item_api.json()
-       
+
         # self.response.write()
-         
+
         # finds product information from ebay's database using the upc code from Walmart
         ebay_item_api = requests.get('http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByProduct&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=ScottMos-Comparif-PRD-1ed499e41-3b97c9fb&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=2&productId.@type=UPC&productId=%s' % (walmart_upc))
         ebay_item_json = ebay_item_api.json()
         # self.response.write(ebay_item_json)
 
         template_dict = {'description': walmart_item_json[u'items'][0][u'shortDescription'], 'image':
-        walmart_json[u'items'][0][u'imageEntities'][0][u'largeImage'], 'price': walmart_item_json[u'items'][0][u'salePrice'], 'name': walmart_item_json[u'items'][0][u'name'], 
+        walmart_json[u'items'][0][u'imageEntities'][0][u'thumbnailImage'], 'price': walmart_item_json[u'items'][0][u'salePrice'], 'name': walmart_item_json[u'items'][0][u'name'],
         }
         self.response.write(result_template.render(template_dict))
-       
-        
+
+
 
 app = webapp2.WSGIApplication([
     ('/', ApiHandler)
-   
+
 ], debug=True)
